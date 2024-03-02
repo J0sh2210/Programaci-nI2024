b1227815a3b8625b831a31057a975c4bb2ca555b
J0sh2210 <132153959+J0sh2210@users.noreply.github.com>
2024-03-02 11:02:45 -0600

    Add files via upload

---
+++ PrimerParcial/Primer_parcial_ejercicio_uno/Primer_parcial_ejercicio_uno/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+namespace Primer_parcial_ejercicio_uno
+{ 
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            String opcion = "";
+            while (opcion != "4")
+            {
+                Console.WriteLine("Bienvenido al cajero automatico");
+                Console.WriteLine("Escriba '1'  si quiere ver su saldo");
+                Console.WriteLine("Escriba '2' si quiere depositar dinero");
+                Console.WriteLine("Escriba '3' si quiere retirar dinero");
+                Console.WriteLine("Escriba '4' si quiere salir del programa");
+                opcion = Console.ReadLine();
+                int dinero = 0;
+                /* while (opcion >4)
+                 {
+
+                     if (opcion == 1)
+                     {
+                         Console.WriteLine("Escriba la cantidad a ingresar");
+                         int cantidad = Console.Read();
+                         dinero = dinero + cantidad;
+
+                     }if (opcion == 2)
+                     {
+                         Console.WriteLine("ingrese cantidad a retirar");
+                         int retirar = Console.Read();
+
+                     }
+                     if (opcion == 3)
+                     {
+                         Console.WriteLine("la cantidad de dinero que posee es: " + dinero);
+                     }
+
+
+                     } */
+                switch (opcion)
+                {
+                    case "1":
+                        Console.WriteLine("ingrese cantidad a depositar");
+                        int deposito = Console.Read();
+                        dinero = dinero + deposito;
+                        break;
+                    case "2":
+                        Console.WriteLine("Ingrese cantidad a retirar");
+                        int retiro = Console.Read();
+                        dinero = dinero - retiro;
+                        break;
+                    case "3":
+                        Console.WriteLine("Su saldo es " + dinero);
+                        break;
+
+
+                }
+            }
+            }
+
+
+
+        }
+
+    }
+
---
+++ PrimerParcial/Primer_parcial_ejercicio_uno/Suma_numeros/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+namespace Suma_numeros
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+            int suma = Console.Read();
+            int Num = 0;
+           
+            
+                if (Num != 0)
+                {
+                    
+                    Console.WriteLine("Ingrese un nuevo número");
+                    suma = Console.Read();
+                    Num = Num + suma;
+                } else
+                {
+                    Console.WriteLine("La suma de los numeros es: " + Num);
+                }
+            
+          
+        }
+    }
+}
