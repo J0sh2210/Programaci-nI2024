e267e0d22486d88b8406cf7d1afff39b0bb15a64
J0sh2210 <132153959+J0sh2210@users.noreply.github.com>
2024-03-15 23:54:40 -0600

    Add files via upload

---
+++ Semana_6_desafios/Promedio/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace Semana_6_desafios
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int nota;
+            float contador = 0;
+            string fin;
+            float nota_almacenada = 0 ;
+            do
+            {
+                Console.WriteLine("Ingrese sus notas");
+                nota = int.Parse(Console.ReadLine());
+                contador++;
+                nota_almacenada = nota_almacenada + nota;
+                Console.WriteLine("Escriba 'FIN' si desea terminar de ingresar notas y 'NO' si quiere seguir escribiendo notas");
+                fin = Console.ReadLine();
+                if (fin == "FIN")
+                {
+                    Console.WriteLine("El promedio de las notas es: " + nota_almacenada/contador);
+                    break;
+                }if (fin == "NO")
+                {
+                    continue;
+                }
+            } while (nota >= 0 && nota <= 10);
+        }
+    }
+}
---
+++ Semana_6_desafios/Semana_6_desafios/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Semana_6_desafios
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Escriba un número positivo");
+            int numero = int.Parse(Console.ReadLine());
+                if (numero < 0 ) {
+
+                Console.WriteLine("Numero no válido");
+                    }else
+            {
+                for (int i = 1; i <= numero; i++)
+                {
+                    if (i % 2 != 0)
+                    {
+                        continue;
+                    }
+                    else
+                    {
+                        Console.WriteLine(i);
+                    }
+
+                }
+            }
+        }
+    }
+}
