a49d61c985266862546acb84a135d234adbfbd1c
J0sh2210 <132153959+J0sh2210@users.noreply.github.com>
2024-03-22 23:27:28 -0600

    Add files via upload

---
+++ sesion_7/Descuentos/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace sesion_7
+{
+    internal class Descuentos
+    {
+        static void Main(string[] args)
+        {
+            double[,] compras =  {
+                {10, 500, 300, 20, 30, },
+                {1000, 200, 40, 2, 3, },
+                {2, 30, 5, 4, 1 },
+                {40, 50, 50, 70, 40 },
+                {500, 2300, 432, 322, 32 }
+
+            };
+            Aplicardescuento(compras);
+        }
+
+            static void Aplicardescuento(double[,] compras)
+            {
+                for (int clien = 0; clien < compras.GetLength(0); clien++) {
+                    double compratotal = 0;
+                    for (int compra = 0;  compra < compras.GetLength(1); compra++)
+                    {
+                        compratotal += compras[clien, compra];
+                    }
+                    double descuento = 0;
+                    if (compratotal >= 100 && compratotal <= 1000) {
+                        descuento = 0.1;
+                    } else if (compratotal > 1000)
+                    {
+                        descuento = 0.2;
+                    }
+                    double total = compratotal - (compratotal * descuento);
+                    Console.WriteLine($"El cliente número {clien + 1}");
+                    Console.WriteLine($"La compra fue de: { compratotal}");
+                    Console.WriteLine($"Descuento que se aplico: {(descuento * 100) : 0}%");
+                    Console.WriteLine($"Total con descuento: {compratotal}");
+                    Console.WriteLine();
+                }
+                
+            } 
+
+        
+        
+    }
+}
---
+++ sesion_7/sesion_7/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+namespace sesion_7
+{
+    internal class Totito
+    {
+        static void Main(String[] args)
+        {
+            const int filas = 3;
+            const int columnas = 3;
+            string movimiento;
+            String[,] tablero = new String[filas, columnas];
+            void imprimir () {
+                for (int i = 0; i < filas; i++)
+                {
+                    for (int j = 0; j < columnas; j++)
+                    {
+                        tablero[i, j] = "-";
+                    }
+                }
+            }
+            void consola_print ()
+            {
+                for(int i = 0;i < filas; i++)
+                {
+                    for(int j = 0; j < columnas;j++)
+                    {
+                        Console.Write(tablero[i, j]);
+                    }
+                    Console.WriteLine();
+                }
+            }
+            imprimir ();
+
+            
+            for (int i = 0; i < filas; i++)
+            {
+                for (int j = 0; j < columnas; j++)
+                {
+                    Console.WriteLine("Ingrese su movimiento");
+                    movimiento = Console.ReadLine();
+                    tablero[i, j] = movimiento;
+                    consola_print();
+                }
+            }
+            for (int i = 0; i < filas; i++)
+            {
+                for (int j = 0; j < columnas; j++)
+                {
+                    Console.Write(tablero[i, j] + " ");
+                }
+                Console.WriteLine();
+            }
+        }
+
+       
+
+    }
+}
