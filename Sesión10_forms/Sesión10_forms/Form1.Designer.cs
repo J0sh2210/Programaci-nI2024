91f153949ce012ecd4ad49719f7ec87e1a443ae0
J0sh2210 <132153959+J0sh2210@users.noreply.github.com>
2024-04-19 23:44:12 -0600

    Add files via upload

---
+++ Sesión10_forms/Sesión10_forms/Form1.Designer.cs
@@ -0,0 +1,81 @@
+﻿namespace Sesión10_forms
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            textBox = new TextBox();
+            Barra1 = new TrackBar();
+            ((System.ComponentModel.ISupportInitialize)Barra1).BeginInit();
+            SuspendLayout();
+            // 
+            // textBox
+            // 
+            textBox.AcceptsReturn = true;
+            textBox.BackColor = SystemColors.InactiveCaption;
+            textBox.Location = new Point(357, 69);
+            textBox.Multiline = true;
+            textBox.Name = "textBox";
+            textBox.Size = new Size(234, 153);
+            textBox.TabIndex = 0;
+            textBox.TextAlign = HorizontalAlignment.Center;
+            textBox.TextChanged += textBox1_TextChanged;
+            // 
+            // Barra1
+            // 
+            Barra1.AllowDrop = true;
+            Barra1.BackColor = SystemColors.ButtonFace;
+            Barra1.Cursor = Cursors.VSplit;
+            Barra1.Location = new Point(47, 347);
+            Barra1.Maximum = 100;
+            Barra1.Name = "Barra1";
+            Barra1.Size = new Size(886, 45);
+            Barra1.TabIndex = 1;
+            Barra1.Scroll += Barra1_Scroll;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = SystemColors.ButtonFace;
+            ClientSize = new Size(961, 450);
+            Controls.Add(Barra1);
+            Controls.Add(textBox);
+            Name = "Form1";
+            Text = "Sesion10";
+            Load += Form1_Load;
+            ((System.ComponentModel.ISupportInitialize)Barra1).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private TextBox textBox;
+        private TrackBar Barra1;
+    }
+}
---
+++ Sesión10_forms/Sesión10_forms/Form1.cs
@@ -0,0 +1,24 @@
+namespace Sesión10_forms
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        public void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+        private void Barra1_Scroll(object sender, EventArgs e)
+        {
+        }
+      
+    }
+}
