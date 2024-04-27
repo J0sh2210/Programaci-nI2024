ffb6d5b7596ba4434e52811e84849634fc330182
J0sh2210 <132153959+J0sh2210@users.noreply.github.com>
2024-04-27 11:08:48 -0600

    Add files via upload

---
+++ segun_parcial/Calculadora.cs
@@ -0,0 +1,162 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Resources;
+
+namespace Segundo_Parcial
+{
+    public class Calculadora : INotifyPropertyChanged
+    {
+        private string num1;
+        private string num2  ;
+        private string resultado;
+        public string Num1
+        {
+            get
+            {
+                return num1;
+            }
+            set
+            {
+                if (decimal.TryParse(value, out var _))
+                {
+                    num1 = value;
+                }
+                OnPropertyChanged("num1");
+                OnPropertyChanged("resultado");
+            }
+        }
+
+        public string Num2
+        {
+            get
+            {
+                return num2;
+            }
+            set
+            {
+                if (decimal.TryParse(value, out var _))
+                {
+                    num2 = value;
+                }
+                OnPropertyChanged("num2");
+                OnPropertyChanged("resultado");
+            }
+        }
+
+        public string Sum
+        {
+            get
+            {
+                return (int.Parse(Num1) + int.Parse(Num2)).ToString();
+            }
+            set
+            {
+                resultado = (int.Parse(Num1) + int.Parse(Num2)).ToString();
+                OnPropertyChanged("resultado");
+            }
+        }
+        public string Rest
+        {
+            get
+            {
+                return (int.Parse(Num1) - int.Parse(Num2)).ToString();
+            }
+            set
+            {
+                resultado = (int.Parse(Num1) - int.Parse(Num2)).ToString();
+                OnPropertyChanged("resultado");
+            }
+        }
+        public string Mult
+        {
+            get
+            {
+                return (int.Parse(Num1) * int.Parse(Num2)).ToString();
+            }
+            set
+            {
+                resultado = (int.Parse(Num1) * int.Parse(Num2)).ToString();
+                OnPropertyChanged("resultado");
+            }
+        }
+        public string Div
+        {
+            get
+            {
+                return (int.Parse(Num1) / int.Parse(Num2)).ToString();
+            }
+            set
+            {
+                resultado = (int.Parse(Num1) / int.Parse(Num2)).ToString();
+                OnPropertyChanged("resultado");
+            }
+        }
+
+        public string Raiz_cuadradra
+        {
+            get
+            {
+                return(int.Parse(Num1.Sqrt).ToString;
+            }
+        }
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        private void OnPropertyChanged(string propiedad)
+        {
+            if (this.PropertyChanged != null)
+            {
+                this.PropertyChanged(this, new PropertyChangedEventArgs(propiedad));
+            }
+        }
+       /* private string num1;
+        private string num2;
+        private string resultado;
+        public string Num1
+        {
+            get { return num1; }
+            set
+            {
+                num1 = value;
+               
+            }
+
+        }
+        public string Num2
+        {
+            get
+            {
+                return num1;
+            }
+            set { num1 = value; }
+        }
+        public string Resultado
+        {
+            get
+            {
+                return (int.Parse(Num1) + int.Parse(Num2)).ToString();
+            }
+            set
+            {
+                resultado = (int.Parse(Num1) + int.Parse(Num2)).ToString();
+
+
+            }
+
+        } */
+
+       
+
+      
+    }
+}
+
+    
+
---
+++ segun_parcial/MainWindow.xaml.cs
@@ -0,0 +1,58 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Segundo_Parcial
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+
+        }
+        public void Suma(object sender, EventArgs e)
+        {
+
+            Calculadora calculadora = new Calculadora{Num1 = "1", Num2= "3"};
+            this.DataContext = calculadora;
+        }
+        public void Resta (object sender, EventArgs e)
+        {
+
+            Calculadora calculadora = new Calculadora { Num1 = "1", Num2 = "3" };
+            this.DataContext = calculadora;
+        }
+        public void Multiplicacion (object sender, EventArgs e)
+        {
+
+            Calculadora calculadora = new Calculadora { Num1 = "1", Num2 = "3" };
+            this.DataContext = calculadora;
+        }
+        public void Division (object sender, EventArgs e)
+        {
+
+            Calculadora calculadora = new Calculadora { Num1 = "1", Num2 = "3" };
+            this.DataContext = calculadora;
+
+        }
+        public void Raiz (object sender, EventArgs e)
+        {
+            Calculadora calculadora = new Calculadora { Num1 = "1" };
+            this.DataContext = calculadora;
+        }
+
+
+    }
+}
